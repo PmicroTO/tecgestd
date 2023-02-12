af95c1ae735011f29620370137fe46952e300bcf
Lucio Araujo <iyw6gc@gmail.com>
2023-02-12 08:18:44 -0300

    adicioado scripts de criacao, mudanca no diagrama logico e no print

---
+++ cead006/gen_mercado.sql
@@ -0,0 +1,11 @@
+
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(34381882000102, 'Assai Atacadista', 6477175, 'Piauí', 'Teresina', 'Av. José Francisco de Almeida Neto', '11');
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(53984709000135, 'Mercado Extra', 64019870, 'Piauí', 'Teresina', 'Av. Barão de Gurguéia', '3191');
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(26484118000126, 'Carvalho Super Shopping da Cidade', 64000010, 'Piauí', 'Teresina', 'Av. Maranhão, 300 - 1º andar Loja 1', '1');
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(42631540000113, 'Kaique e Gael Comercio Ltda', 64258987, 'Piauí', 'Teresina', '02', '5874');
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(55287224000118, 'Caroline e Fábio comercio Ltda', 64071450, 'Piauí', 'Teresina', 'São Francisco', '2415');
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(95058542000121, 'Aurora e Cauê Adega Ltda', 64200005, 'Piauí', 'Teresina', 'Paraíba', '566');
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(3309811000167, 'Bernardo e Benedito comercio ME', 64604210, 'Piauí', 'Teresina', 'Projetada', '203');
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(48807136000134, 'Igor e Thiago Buffet ME', 64059255, 'Piauí', 'Teresina', 'Doutor João Silva Filho', '55');
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(5995436000181, 'Mini Mercado Lindolfo Monteiro ', 64052810, 'Piauí', 'Teresina', 'Av. Lindolfo Monteiro', '1867');
+INSERT INTO listapopular.mercado (cnpjmer, nome, cep, estado, cidade, rua, numero) VALUES(80421445000184, 'Mix Mateus Atacarejo', 64020720, 'Piauí', 'Teresina', 'Av. Henry Wall de Carvalho', '5300A');
